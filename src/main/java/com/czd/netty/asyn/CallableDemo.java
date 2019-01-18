f156da77820ce8d51cb91ab9c5cff5a9c4f910d6
changzhendong <changzhendong@daojia-inc.com>
2019-01-18 13:50:11 +0800

    doc: workspace init

---
+++ src/main/java/com/czd/netty/asyn/CallableDemo.java
@@ -0,0 +1,47 @@
+package com.czd.netty.asyn;
+
+/**
+ * @Author changzhendong
+ * @Description: 异步的一种实现方式就是 回调
+ * @Date: Created in 2019/1/16 19:51.
+ */
+public class CallableDemo {
+
+	public static void main(String[] args) {
+		IFetcher fetcher = new Fetcher();
+		fetcher.fetchData(new FetchCallable() {
+			@Override
+			public void onData() throws Exception {
+				System.out.println("data is OK");
+			}
+			@Override
+			public void onError() {
+				System.out.println("data is error");
+			}
+		});
+	}
+
+	interface FetchCallable {
+		void onData() throws Exception;
+		void onError();
+	}
+
+	interface IFetcher {
+		void fetchData(FetchCallable callable);
+	}
+
+	static class Fetcher implements IFetcher {
+		@Override
+		public void fetchData(FetchCallable callable) {
+			try {
+
+				// todo something
+				System.out.println("模拟远程调用 暂停 5s 中");
+				Thread.sleep(5000);
+				callable.onData();
+			} catch (Exception e) {
+				callable.onError();
+			}
+		}
+	}
+}
---
+++ src/main/java/com/czd/netty/asyn/FutureDemo.java
@@ -0,0 +1,48 @@
+package com.czd.netty.asyn;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author changzhendong
+ * @Description:
+ * @Date: Created in 2019/1/16 20:15.
+ */
+public class FutureDemo {
+	private static ExecutorService executorService = Executors.newCachedThreadPool();
+
+	public static void main(String[] args) throws Exception {
+		Runnable task1 = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("i am task1");
+			}
+		};
+
+		Callable<Integer> task2 = new Callable<Integer>() {
+			@Override
+			public Integer call() throws Exception {
+				System.out.println("i am task2");
+				return 10;
+			}
+		};
+
+		Future f1 = executorService.submit(task1);
+		Future<Integer> f2 = executorService.submit(task2);
+		System.out.println("task1 is complete:" + f1.isDone());
+		System.out.println("task2 is complete:" + f2.isDone());
+		// wait
+		TimeUnit.SECONDS.sleep(1);
+		while (!f1.isDone()) {
+			System.out.println("wait for task1");
+		}
+		System.out.println("task1 is complete.");
+		while (!f2.isDone()) {
+			System.out.println("wait for task2");
+		}
+		System.out.println("task2 is complete return value:" + f2.get());
+	}
+}
